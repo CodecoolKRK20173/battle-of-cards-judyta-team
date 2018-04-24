0cadcb1703afad1fd986cfb4d2d3b1130df9302b
Cyan <dawid.grygier5@gmail.com>
2018-04-24 13:22:39 +0200

    merge

---
+++ JudytaBot.java
@@ -0,0 +1,12 @@
+public class JudytaBot extends Player {
+
+    public JudytaBot(int cash) {
+        super("Judyta", cash);
+    }
+
+    @Override
+    public void keepHand() {
+        //game over;
+    }
+
+}
