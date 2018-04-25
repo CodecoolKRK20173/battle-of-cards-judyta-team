cd18e1d74bc3b8cc49bcf6de64c1b0316e8ed3b5
Roman <adam.romanowski93@gmail.com>
2018-04-25 08:19:24 +0200

    Implement table() method

--- Display.java
+++ Display.java
@@ -8,5 +8,18 @@ public class Display {
         this.players = players;
     }
 
+    public void table() {
+        PlayerIterator iterator = new PlayerIterator(players);
 
+        while (iterator.hasNext()) {
+            
+            Player p = iterator.next();
+            StringBuilder playerItems = new StringBuilder();
+
+            playerItems.append("Name: " + p.getName())
+                       .append(" Coins: " + p.getCoolcoin())
+                       .append(" Cards: " + p.getHand());
+
+        }
+    }
 }
