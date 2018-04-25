c04f53a1b6b328721e5023f2946cea06fb339850
Roman <adam.romanowski93@gmail.com>
2018-04-25 08:12:39 +0200

    Initialize fields, constructor

---
+++ Display.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Display {
+
+    private ArrayList<Player> players;
+
+    public Display(ArrayList<Player> players) {
+        this.players = players;
+    }
+
+
+}
