603dfac148759f14bddcd2ecba6a0f2ca0e9cfa7
Roman <adam.romanowski93@gmail.com>
2018-04-26 03:13:58 +0200

    DisplayFeature finished

--- Display.java
+++ Display.java
@@ -29,11 +29,12 @@ public class Display {
         Player judyta = players.get(judytaIndex);
         StringBuilder table = new StringBuilder();
 
-        table.append(this.formatted(judyta.getName(), tableWidth) + "\n")    // Name
-             .append(this.formatted("Coins: " + judyta.getCoolcoin(), tableWidth) + "\n")
-             .append(this.formattedCards(tableWidth) + "\n")    // Coins
-             .append(this.formatted("Score: " + judyta.getScore(), tableWidth) + "\n")   // Score
-             .append(this.formatted("Total bet: " + totalBet, tableWidth) + "\n\n\n");   // Total bet
+        table.append(this.formattedBot(judyta.getName(), (tableWidth / 2)) + "\n")    // Name
+             .append(this.formattedBot("Coins: " + judyta.getCoolcoin(), (tableWidth / 2)) + "\n\n")   // Coins
+             .append(String.format("%1$-" + (tableWidth / 2) + "s", " ")).append(" ") //  Tactical space
+             .append(this.formattedCards((tableWidth / 2), judyta) + "\n\n")    // Cards
+             .append(this.formattedBot("Score: " + judyta.getScore(), (tableWidth / 2)) + "\n\n\n\n\n\n")   // Score
+             .append(this.formattedBot("Total bet: " + totalBet, (tableWidth / 2)) + "\n\n\n\n\n\n");   // Total bet
 
 
         table.append(this.playersData());
@@ -49,7 +50,6 @@ public class Display {
 
         ArrayList<String> names = new ArrayList<>();
         ArrayList<Integer> coins = new ArrayList<>();
-        ArrayList<Pile> cards = new ArrayList<>();
         ArrayList<Integer> scores = new ArrayList<>();
 
         PlayerIterator iterator = new PlayerIterator(humanPlayers);
@@ -58,60 +58,61 @@ public class Display {
             Player player = iterator.next();
             names.add(player.getName());
             coins.add(player.getCoolcoin());
-            cards.add(player.getHand());
             scores.add(player.getScore());
         }
 
         for (String n : names) {
-            playersData.append(formatted(n, MAX_PLAYER_WIDTH));
+            playersData.append(formattedHuman(n, MAX_PLAYER_WIDTH));
         }
 
         playersData.append("\n");
 
         for (Integer c : coins) {
-            playersData.append(formatted("Coins: " + c, MAX_PLAYER_WIDTH));
+            playersData.append(formattedHuman("Coins: ".concat(c.toString()), MAX_PLAYER_WIDTH));
         }
 
-        playersData.append("\n");
+        playersData.append("\n\n");
 
-        for (Pile c : cards) {
-            playersData.append(formatted(c.toString(), MAX_PLAYER_WIDTH));
+        for (Player player : humanPlayers) {
+            playersData.append(formattedCards(MAX_PLAYER_WIDTH, player));
         }
 
-        playersData.append("\n");
+        playersData.append("\n\n");
 
         for (Integer s : scores) {
-            playersData.append(formatted("Score: " + s, MAX_PLAYER_WIDTH));
+            playersData.append(formattedHuman("Score: " + s.toString(), MAX_PLAYER_WIDTH));
         }
 
         return playersData.toString();
     }
 
-    private String formatted(String content, int width) {
+    private String formattedBot(String content, int width) {
         
-        return String.format("%1$-" + (width / 2)
-                            + "s %2$-" + (width / 2)
-                            + "s"
+        return String.format("%1$-" + width + "s "
+                            + "%2$-" + width + "s"                            
                             , " "
                             , content);
 
     }
 
-    private String formattedCards(int width) {
+    private String formattedHuman(String content, int width) {
 
-        Player judyta = players.get(judytaIndex);
-        List<Card> cards = judyta.getHand().getAllCards();
+        return String.format("%1$-" + width + "s", content);
+    }
+
+    private String formattedCards(int width, Player player) {
+
+        List<Card> cards = player.getHand().getAllCards();
 
         StringBuilder cardsFormat = new StringBuilder();
-
-        cardsFormat.append(String.format("%1$-" + (width / 2) + "s", " ")).append(" ");
 
         for (Card c : cards) {
             cardsFormat.append(String.format("%1$c", c.getCodePointValue()));
             cardsFormat.append(" ");
         }
 
-        cardsFormat.append(String.format("%1$-" + (MAX_PLAYER_WIDTH - (cards.size() * 2)) + "s", " "));
+        cardsFormat.append(String.format("%1$-" + (width - (cards.size() * 2) - 1) + "s", " "))
+                   .append(" ");
 
         return cardsFormat.toString();
     }
